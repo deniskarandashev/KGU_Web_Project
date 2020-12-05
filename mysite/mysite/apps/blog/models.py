090b88688db3064e45316a2532e8bbdfa45f49ec
“Denis <denis.karandashev@gmail.comgit config --global user.emailssh-keygen -t rsa -C denis.karandashev@gmail.com>
2020-12-05 18:57:57 +0300

    Add tables Articles and Comments, create superuser and add Aticles and Comments to an admin page

--- mysite/mysite/apps/blog/models.py
+++ mysite/mysite/apps/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField('название статьи', max_length= 120)
+    body = models.TextField('текст статьи')
+    date = models.DateTimeField('дата публикации') 
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.CharField('имя автора', max_length=30)
+    comment_text = models.CharField('текст комментария', max_length=220)   
--- mysite/mysite/apps/blog/views.py
+++ mysite/mysite/apps/blog/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request) :
     return HttpResponse("Это работает!")
+
+def second(request) :
+    return HttpResponse("Это тоже работает!")
