e3f256ac9e3a4186c81bc933ae12128eaee24081
“Denis <denis.karandashev@gmail.comgit config --global user.emailssh-keygen -t rsa -C denis.karandashev@gmail.com>
2020-12-09 19:42:44 +0300

    Add images and texts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 #PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_ROOT = ''
