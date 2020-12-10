ca3db3eb1cc3f42f841dd373cb78d92551695175
“Denis <denis.karandashev@gmail.comgit config --global user.emailssh-keygen -t rsa -C denis.karandashev@gmail.com>
2020-12-10 18:51:35 +0300

    Add single page

--- mysite/mysite/apps/blog/urls.py
+++ mysite/mysite/apps/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:article_id>', views.single, name='single'),
 ]
--- mysite/mysite/apps/blog/views.py
+++ mysite/mysite/apps/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from blog.models import Article
+from django.http import Http404, HttpResponseRedirect
 
 # Create your views here.
 #from django.http import HttpResponse
@@ -9,12 +11,13 @@ from django.shortcuts import render
 # def second(request) :
 #    return HttpResponse("Это тоже работает!")
 
-
 def index(request):
-    return render(
-        request, 
-        'blog/index.html', 
-        {
+    list_articles = Article.objects.order_by('-date') # вывод статей от более новой до старой
+    return render(request, 'blog/test.html', {'list_articles': list_articles} ) # заменить на blog/index.html
 
-        }
-    )
+def single(request, article_id):
+    try:
+        a = Article.objects.get(id = article_id)
+    except:
+        raise Http404("Article doesn't find")
+    return render(request, 'blog/single.html', {'article': a})
