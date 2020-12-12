c6f86cb914558ab65f23ac7985c8171b214b5836
“Denis <denis.karandashev@gmail.comgit config --global user.emailssh-keygen -t rsa -C denis.karandashev@gmail.com>
2020-12-12 18:03:47 +0300

    Add postgeSQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_blog',
-        'USER' : 'posgres',
+        'USER' : 'postgres',
         'PASSWORD' : '123',
         'HOST' : '127.0.0.1',
         'PORT' : '5432',
